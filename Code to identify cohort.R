620d23a87409d9d0ec39fe57e295b7334994a005
epistreams <epistreams@outlook.com>
2022-01-30 17:25:11 -0500

    added valid BP requirement. Still need to figure out how to capture those who had information on HTN medication use...

--- Code to identify cohort.R
+++ Code to identify cohort.R
@@ -22,11 +22,17 @@ library(tidyverse)
 # Replicating Muntner's Cohorts -------------------------------------------
 
 # 2015-2016
-n1516_1 <- read_rds("rds_data/demo_i")
-nb1516 <- read_rds("rds_data/bpx_i")
+demo_1516 <- read_rds("rds_data/demo_i")
+bpm_1516 <- read_rds("rds_data/bpx_i")
+bpq_1516 <- read_rds("rds_data/bpq_i")
 
-n1516_2 <- n1516_1 %>% 
+n1516_1 <- demo_1516 %>% 
   filter(WTMEC2YR > 0) %>% 
   filter(RIDAGEYR >= 18) %>% 
   filter(RIDEXPRG != 1 | is.na(RIDEXPRG)) %>% 
-  left_join(nb1516, by = "SEQN")
+  left_join(bpm_1516, by = "SEQN") %>% 
+  mutate(
+    sbp_sum = rowSums(.[c("BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4")], na.rm = TRUE),
+    dbp_sum = rowSums(.[c("BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4")], na.rm = TRUE)) %>% 
+  filter(sbp_sum > 0 & dbp_sum > 0) %>% 
+  left_join(bpq_1516, by = "SEQN")
