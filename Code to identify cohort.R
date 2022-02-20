b3ede5ab15d1af9d97165d43b58174da0f402db4
epistreams <epistreams@outlook.com>
2022-02-20 13:05:46 -0500

    streamlined the coding of counts and averages. Also, with respect to DBP, I coded 0 mm Hg readings as missing.
    Now that this is done, next step is apply my criteria and save the data once again.

--- Code to identify cohort.R
+++ Code to identify cohort.R
@@ -31,18 +31,18 @@ cohort_1516 <- demo_1516 %>%
   filter(RIDAGEYR >= 18) %>% 
   filter(RIDEXPRG != 1 | is.na(RIDEXPRG)) %>% 
   left_join(bpm_1516, by = "SEQN") %>% 
+  mutate_at(vars(starts_with("BPXDI")), list(~na_if(., 0))) %>%
   mutate(
-    sbp_sum = rowSums(.[c("BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4")], na.rm = TRUE),
-    sbp_count = rowSums(!is.na(.[c("BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4")])),
-    dbp_sum = rowSums(.[c("BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4")], na.rm = TRUE),
-    dbp_count = rowSums(!is.na(.[c("BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4")]))
-    ) %>% 
-  filter(sbp_sum != 0 & dbp_sum !=0) %>% 
+    sbp_count = rowSums(!is.na(select(., starts_with("BPXSY")))),
+    dbp_count = rowSums(!is.na(select(., starts_with("BPXDI"))))
+    ) %>%
+  filter(sbp_count > 0 & dbp_count > 0) %>% 
+  mutate(
+    sbp_avg = rowMeans(select(., starts_with("BPXSY")), na.rm = TRUE),
+    dbp_avg = rowMeans(select(., starts_with("BPXDI")), na.rm = TRUE)) %>% 
   left_join(bpq_1516, by = "SEQN") %>% 
   filter(BPQ020 != 9) %>% 
   mutate(
-    sbp_avg = sbp_sum/sbp_count,
-    dbp_avg = dbp_sum/dbp_count,
     htn_bp = if_else(sbp_avg < 140 & dbp_avg < 90, true =  0, false = 1),
     htn_meds = case_when(
                           BPQ020 == 2 ~ 0,
@@ -67,18 +67,18 @@ cohort_1718 <- demo_1718 %>%
   filter(RIDAGEYR >= 18) %>% 
   filter(RIDEXPRG != 1 | is.na(RIDEXPRG)) %>% 
   left_join(bpm_1718, by = "SEQN") %>% 
+  mutate_at(vars(starts_with("BPXDI")), list(~na_if(., 0))) %>%
   mutate(
-    sbp_sum = rowSums(.[c("BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4")], na.rm = TRUE),
-    sbp_count = rowSums(!is.na(.[c("BPXSY1", "BPXSY2", "BPXSY3", "BPXSY4")])),
-    dbp_sum = rowSums(.[c("BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4")], na.rm = TRUE),
-    dbp_count = rowSums(!is.na(.[c("BPXDI1", "BPXDI2", "BPXDI3", "BPXDI4")]))
-  )%>% 
-  filter(sbp_sum != 0 & dbp_sum !=0) %>% 
+    sbp_count = rowSums(!is.na(select(., starts_with("BPXSY")))),
+    dbp_count = rowSums(!is.na(select(., starts_with("BPXDI"))))
+  ) %>%
+  filter(sbp_count > 0 & dbp_count > 0) %>% 
+  mutate(
+    sbp_avg = rowMeans(select(., starts_with("BPXSY")), na.rm = TRUE),
+    dbp_avg = rowMeans(select(., starts_with("BPXDI")), na.rm = TRUE)) %>% 
   left_join(bpq_1718, by = "SEQN") %>% 
   filter(BPQ020 != 9) %>% 
   mutate(
-    sbp_avg = sbp_sum/sbp_count,
-    dbp_avg = dbp_sum/dbp_count,
     htn_bp = if_else(sbp_avg < 140 & dbp_avg < 90, true =  0, false = 1),
     htn_meds = case_when(
       BPQ020 == 2 ~ 0,
