227a5c1219fbe7a2a8747766fd33f4e64bc598c6
epistreams <epistreams@outlook.com>
2022-04-28 22:39:11 -0400

    Fixing yesterday's mistake by restricting the saved data frames to contain only the indicator variable, identifier, and htn variables. I will merge in the variables from the demographic data frames later. From there I can do the analysis.

--- analysis code.R
+++ analysis code.R
@@ -7,3 +7,17 @@ install.packages("srvyr")
 library(tidyverse)
 library(survey)
 library(srvyr)
+
+
+#creating stacked demographic data frame
+demo_1516 <- read_rds("rds_data/demo_i") %>% 
+  select(SEQN, SDDSRVYR, RIAGENDR, RIDAGEYR, RIDRETH3, DMDEDUC2, WTMEC2YR, SDMVPSU, SDMVSTRA, INDFMPIR)
+demo_1718 <- read_rds("rds_data/demo_j") %>% 
+  select(SEQN, SDDSRVYR, RIAGENDR, RIDAGEYR, RIDRETH3, DMDEDUC2, WTMEC2YR, SDMVPSU, SDMVSTRA, INDFMPIR)
+demographics <- bind_rows(
+  demo_1516,
+  demo_1718
+)
+
+
+#next step is to merge each cohort with the stacked demographic data, which will create the analysis data frames
